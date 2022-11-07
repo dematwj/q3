bee1a7fa17608d897ad3baa1c0a14d11139032ca
dematwj <114079784+dematwj@users.noreply.github.com>
2022-11-07 17:20:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const bodyParser = require("body-parser");
 const api = require("./routes/api");
 
 const app = express();
-const PORT = 5000;
+const PORT = 400;
 
 app.use(bodyParser.json());
 app.use("/", api);
 
 app.listen(PORT, () => {
-  console.log("Server listening on port ", PORT);
+  console.log("Server listening ====== on port ", PORT);
 });
